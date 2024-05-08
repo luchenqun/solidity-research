f7bd9896bf2c824bc0a6678ab58cbedb7f827d2d
Luke <luchenqun@qq.com>
2024-05-08 16:22:52 +0800

    update PreCompileBug.sol

--- contracts/PreCompileBug.sol
+++ contracts/PreCompileBug.sol
@@ -2,78 +2,28 @@
 
 pragma solidity ^0.8.0;
 
-enum BondStatus {
-    Unspecified,
-    Unbonded,
-    Unbonding,
-    Bonded
-}
-
-/// @dev Represents a validator in the staking module.
-struct Validator {
-    string operatorAddress;
-    string consensusPubkey;
-    bool jailed;
-    BondStatus status;
-    uint256 tokens;
-    uint256 delegatorShares; // TODO: decimal
-    string description;
-    int64 unbondingHeight;
-    int64 unbondingTime;
-    uint256 commission;
-    uint256 minSelfDelegation;
-}
-
 interface StakingI {
-    function delegate(
-        address delegatorAddress,
-        string memory validatorAddress,
-        uint256 amount
-    ) external returns (bool success);
-
+    // 注意原来的预编译合约接口返回的是一个Validator结构体数据
+    // 这里我简单返回了string，与重现漏洞无关
     function validator(
         string memory validatorAddress
-    ) external view returns (Validator calldata validator);
+    ) external view returns (string calldata validator);
 }
 
 contract PrecompileBug {
-    StakingI public staking =
-        StakingI(0x0000000000000000000000000000000000000800);
     uint256 public count = 0;
 
-    event Result(bool indexed success);
-
-    receive() external payable {}
-
-    constructor() payable {}
-
-    function setCount1(
-        address delegatorAddress,
-        string memory validatorAddress,
-        uint256 amount
-    ) public {
-        count = 1000;
-        bool success = staking.delegate(
-            delegatorAddress,
-            validatorAddress,
-            amount
+    function setCount() public {
+        count = 255;
+        StakingI staking = StakingI(0x0000000000000000000000000000000000000800);
+        staking.validator(
+            "evmosvaloper1wgmweh3ayhnnpy7lqxd7zll78sjv6kvfms9t8h"
         );
-        // emit Result(success);
         count = 0;
     }
 
-    function setCount2(string memory validatorAddress) public {
-        count = 1000;
-        staking.validator(validatorAddress);
+    // 用于多次测试
+    function resetCount() public {
         count = 0;
-    }
-
-    function delegate(
-        address delegatorAddress,
-        string memory validatorAddress,
-        uint256 amount
-    ) public payable {
-        staking.delegate(delegatorAddress, validatorAddress, amount);
-        payable(delegatorAddress).transfer(amount);
     }
 }
--- scripts/deploy-precompile-bug.js
+++ scripts/deploy-precompile-bug.js
@@ -2,25 +2,15 @@ const hre = require('hardhat');
 
 async function main() {
   const PrecompileBug = await hre.ethers.getContractFactory('PrecompileBug');
-  const delegatorAddress = '0x00000be6819f41400225702d32d3dd23663dd690';
-  const validatorAddress = 'evmosvaloper144e9he9zg4v5tfkypxjvahampewsmal43zt70v';
-  const amount = '1000000000000000000';
-
-  const precompileBug = await PrecompileBug.deploy({ value: amount });
+  const precompileBug = await PrecompileBug.deploy();
   await precompileBug.deployed();
 
   console.log(`PrecompileBug contract deployed to ${precompileBug.address}`);
   let tx;
 
-  tx = await precompileBug.setCount2(validatorAddress);
+  tx = await precompileBug.setCount();
   await tx.wait();
-  console.log(await precompileBug.count());
-
-  // tx = await precompileBug.delegate(delegatorAddress, validatorAddress, amount);
-  // await tx.wait();
-
-  // tx = await precompileBug.delegate(delegatorAddress, validatorAddress, amount);
-  // await tx.wait();
+  console.log('count', await precompileBug.count());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
